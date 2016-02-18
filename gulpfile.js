74dc4d44be6ac2b5c299cea6aafd8e1e856124d7
Jerven Clark Chua <jervenclark@users.noreply.github.com>
2016-02-18 09:44:42 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+  mocha = require('gulp-mocha');
+
+gulp.task('test', function() {
+  return gulp
+    .src('test/**/*.js', {
+      read: false
+    })
+    .pipe(mocha({
+      reporter: 'nyan'
+    }));
+});
