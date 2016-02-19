45d779ed881a8743b3d85ca1a08a7e50ecef7429
jerven clark chua <jervenclark@gmail.com>
2016-02-19 10:09:47 +0800

    add polygon area function

--- geometry/area.js
+++ geometry/area.js
@@ -5,7 +5,8 @@ var numeric = require('../utils/numeric');
 module.exports = function() {
   return {
     quadrilateral: quadrilateral,
-    triangle: triangle
+    triangle: triangle,
+    polygon: polygon
   };
 };
 
@@ -17,8 +18,8 @@ module.exports = function() {
  * @return {numeric}
  */
 function triangle(a, b) {
-	var area = quadrilateral(a,b);
-	return !area ? false : area * 0.5;
+  var area = quadrilateral(a, b);
+  return !area ? false : area * 0.5;
 }
 
 /**
@@ -29,6 +30,23 @@ function triangle(a, b) {
  * @return {numeric}
  */
 function quadrilateral(a, b) {
-  var sides = numeric([a,b]);
-	return !sides ? sides : (sides[0] * sides[1]);
+  var sides = numeric([a, b]);
+  return !sides || (sides[0] + sides[1] === 0) ? false : (sides[0] * sides[1]);
+}
+
+/**
+ * Returns the area of a single, convex polygon
+ *
+ * @param  {numeric} a
+ * @param  {integer} b
+ * @return {numeric}
+ */
+function polygon(a, b) {
+  var args =  numeric([a, b]);
+  if (!args || (args[1] === 0) || !Number.isInteger(args[1])) {
+    return false;
+  } else {
+    var area = (Math.pow(args[0], 2) * args[1]) / (4 * Math.tan(Math.PI / args[1]));
+    return Number(area.toFixed(4));
+  }
 }
--- test/geometry/area.spec.js
+++ test/geometry/area.spec.js
@@ -3,23 +3,23 @@ var should = require('chai').should(),
 
 describe('quadrilateral area', function(){
 
-  it('solves 0   * 0    to  0', function() {
-    area.quadrilateral(0,0).should.equal(0);
+  it('solves 0   * 0     to  false', function() {
+    area.quadrilateral(0,0).should.equal(false);
   });
 
-  it('solves 2   * 2    to  4', function() {
+  it('solves 2   * 2     to  4', function() {
     area.quadrilateral(2,2).should.equal(4);
   });
 
-  it('solves 1.2 * 3.2  to  4.08', function() {
+  it('solves 1.2 * 3.2   to  4.08', function() {
     area.quadrilateral(1.2,3.4).should.equal(4.08);
   });
 
-  it('solves a   * a    to  0', function() {
+  it('solves a   * a     to  false', function() {
     area.quadrilateral('a','g').should.equal(false);
   });
 
-  it('solves "6" * "5"  to  30', function() {
+  it('solves "6" * "5"   to  30', function() {
     area.quadrilateral("6","5").should.equal(30);
   });
 
@@ -27,8 +27,56 @@ describe('quadrilateral area', function(){
 
 describe('triangle area', function(){
 
-  it('solves 3   * 4    to  6', function() {
+  it('solves 0   * 0     to  false', function() {
+    area.triangle(0,0).should.equal(false);
+  });
+
+  it('solves 3   * 4     to  6', function() {
     area.triangle(3,4).should.equal(6);
+  });
+
+  it('solves 1.2 * 3.2   to  2.04', function() {
+    area.triangle(1.2,3.4).should.equal(2.04);
+  });
+
+  it('solves a   * a     to  false', function() {
+    area.triangle('a','g').should.equal(false);
+  });
+
+  it('solves "6" * "5"   to  15', function() {
+    area.triangle("6","5").should.equal(15);
+  });
+
+});
+
+describe('polygon area', function(){
+
+  it('solves 0   * 0     to  false', function() {
+    area.polygon(0,0).should.equal(false);
+  });
+
+  it('solves 10  * 5     to  172.0477', function() {
+    area.polygon(10,5).should.equal(172.0477);
+  });
+
+  it('solves 6.4 * 9     to  253.2075', function() {
+    area.polygon(6.4,9).should.equal(253.2075);
+  });
+
+  it('solves 6.4 * 9.5   to  false', function() {
+    area.polygon(6.4,9.5).should.equal(false);
+  });
+
+  it('solves a   * a     to  false', function() {
+    area.polygon('a','g').should.equal(false);
+  });
+
+  it('solves "4" * "6"   to  41.5692', function() {
+    area.polygon("4","6").should.equal(41.5692);
+  });
+
+  it('solves "4" * "6.4" to  41.5692', function() {
+    area.polygon("4","6.4").should.equal(false);
   });
 
 });
